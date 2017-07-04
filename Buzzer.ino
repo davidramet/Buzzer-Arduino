9f29a70d7befa3e7f9a6b97635ae72980b63e9c2
Ramet-David <david@ramet.ovh>
2017-07-04 14:07:29 +0200

    Add files via upload

---
+++ Buzzer.ino
@@ -0,0 +1,23 @@
+int buzzer =7;
+
+void setup()
+{
+pinMode(buzzer,OUTPUT);
+}
+
+void loop()
+{
+int i;
+
+  while(1)
+  {
+  
+    for(i=0; i<80; i++)
+    {
+      digitalWrite(buzzer, HIGH);
+      delay(2000);
+      digitalWrite(buzzer, LOW);
+      delay(2000);
+    }
+  }
+}
